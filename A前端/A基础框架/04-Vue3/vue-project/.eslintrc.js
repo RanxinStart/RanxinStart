0e7c96e0d3a435750a9a9791722f0848beea7372
RanxinStart <597146348@qq.com>
2021-06-12 11:23:40 +0800

    vue文档更新

--- A前端/A基础框架/04-Vue3/vue-project/.eslintrc.js
+++ A前端/A基础框架/04-Vue3/vue-project/.eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
-  'extends': [
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
+  parser: "vue-eslint-parser",
+  plugins: [
+    'html',
+    'vue',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
--- A前端/A基础框架/04-Vue3/vue-project/src/pages/03-★customRef的使用/index.vue
+++ A前端/A基础框架/04-Vue3/vue-project/src/pages/03-★customRef的使用/index.vue
@@ -1,67 +1,41 @@
 <template>
   <button @click="$router.go(-1)">返回</button>
-  <br>
-  <input v-model="state" type="text"><button @click="update">更新数据</button>
-  <h2>{{state}}</h2>
+  <br />
+  <input v-model="myRef" type="text" />
+  <button>更新数据</button>
+  <h2>{{ myRef }}</h2>
 </template>
 
-<script setup>
-  import {customRef,watch,ref} from "vue";
+<script lang='ts' setup>
+import type { Ref } from 'vue'
 
-  // 定义一个自定义 ref的hook
-  const useCustomRefHook = (value, time = 200) =>{
-    let timeOut;
-    return customRef((track, trigger)=>{
-      //track得到参数  trigger更新视图
-        return {
-          get(){
-            track()
-            return value
-          },
-          set(newValue){
-            // 清除定时器
-            clearTimeout(timeOut)
-            // 定时更新数据
-            timeOut = setTimeout(()=>{
-              value = newValue
-              trigger()
-            },time)
-          }
-        }
-    })
-  }
+// 定义一个自定义 ref的hook
 
-  const state = useCustomRefHook('',0)
-  // const state = ref('')
-
-  watch(state,()=>{
-    // 这是一个自定义对象...
-    console.log(state)
-  })
-
-
-  const update = () =>{
-    let a = state.value
-    console.log(a)
-    setTimeout(()=>{
-      state.value = '?????'
-    })
-    setTimeout(()=>{
-      state.value = '1'
-    })
-    setTimeout(()=>{
-      state.value = '2'
-    })
-    setTimeout(()=>{
-      state.value = '3'
-    })
-    setTimeout(()=>{
-      state.value = a
-    })
-  }
+import { customRef } from 'vue'
+// 1.执行coustomRef传入一个方法 可以得到一个自定义的Ref
+let data = ''
+const myRef = customRef(
+    /** 2.方法可以接受两个值
+     * @name track
+     * @description 跟踪数据 如果没有跟踪数据 get将无法获取到数据
+     * @name trigger
+     * @description 触发渲染数据 如果没有触发渲染 将不会更新视图
+     */
+	(track, trigger)=>{
+    // 3.返回一个包含get和set方法的对象
+    return {
+      get(){
+        // 4.get方法中 使用 track()跟踪数据后返回
+        track()
+        return data
+      },
+      set(value:any){
+        data = value
+        // 5.set方法中 使用 trigger()进行修改数据后的更新
+        trigger()
+      }
+    }
+	}
+)
 
 </script>
-
-<style scoped>
-
-</style>
